c74725a09cffa4fe3f6c34acbf68a9896bb9a49a
unknown <classmatergc336@gmail.com>
2020-12-26 13:21:34 +0545

    first commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+    <footer>
+      <div class="footer-container">
+        <div class="left-col">
+          <img src="footerlogo.png" alt="" class="fotlogo">
+          <div class="social-media">
+            <a href="https://www.facebook.com/manjushreepustak/"  target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+          <p class="rights-text">© 2020 Created By <a href="https://www.youtube.com/channel/UC6LDz6ljP7R5euO7a2342kQ?sub_confirmation=1" target="_blank" class="tech">HelpingTech</a> All Rights Reserved.</p>
+        </div>
+
+        <div class="right-col">
+          <h1>Our Newsletter</h1>
+          <div class="border"></div>
+          <p>Enter Your Email to get our news and updates.</p>
+          <form action="" class="newsletter-form">
+            <input type="text" class="txtb" placeholder="Enter Your Email">
+            <input type="submit" class="sub" value="submit">
+          </form>
+        </div>
+      </div>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+
+ <!-- External JavaScript -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 
+ <!-- Internal CSS -->
+ <link rel="stylesheet" href="./main.css">
+ <link rel="stylesheet" href="./css/clock.css">
+ <link rel="stylesheet" href="./css/footer.css">
+
+ <!-- External CSS -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
+</head>
+<body>
+ <div class="navbar">
+<div class="logo">
+
+</div>
+<a class="btn">
+<span></span>
+<span></span>
+<span></span>
+</a>
+<div class="menu">
+  <a href="./home.php">Home</a>
+  <a href="#">Products</a>
+  <a href="#">FAQ</a>
+  <a href="./login.php">Sign In</a>
+</div>
+
+    <div class="clock">               
+      <div class="display">
+             
+</div>
+</div>
+</div>
+<div class="header">
+ 
+</div>
+
+<script src="./js/clock.js"></script>
+
+<script src="main.js"></script>
+ 
